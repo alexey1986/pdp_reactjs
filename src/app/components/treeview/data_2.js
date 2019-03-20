3b24a9f336831b00c3a1e04e6cfe7e0a0be2bd49
apoleshchuk <em167bm09021986Bm>
2019-03-20 11:00:54 +0300

    'example'

---
+++ src/app/components/treeview/data_2.js
@@ -0,0 +1,35 @@
+/*
+var tree = {
+  root: {
+    name: 'asdasd',
+    type: 'folder',
+    description: dataId,
+    children: [
+      'id2',
+      'id2',
+    ],
+  },
+  id1: {
+    name: 'asdasd',
+    type: 'folder',
+    description: 'sdfsdfsd',
+    children: [
+      'id2',
+      'id2',
+    ],
+  },
+  id2: {
+    name: 'asdasd',
+    type: 'folder',
+    description: 'sdfsdfsd',
+    children: [
+      'id4',
+      'id6',
+    ],
+  },
+  id3: {
+    name: 'asdasd',
+    type: 'file',
+    description: 'sdfsdfsd',
+  },
+}*/
