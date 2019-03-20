d4d8575e7a3e26707b90ca0280fca3397fa5a4b8
apoleshchuk <em167bm09021986Bm>
2019-03-20 11:05:04 +0300

    fix

--- docs/index_bundle.js
+++ docs/index_bundle.js
@@ -48426,41 +48426,6 @@ var treeView = {
     }]
   }]
 };
-/*
-var tree = {
-  root: {
-    name: 'asdasd',
-    type: 'folder',
-    description: dataId,
-    children: [
-      'id2',
-      'id2',
-    ],
-  },
-  id1: {
-    name: 'asdasd',
-    type: 'folder',
-    description: 'sdfsdfsd',
-    children: [
-      'id2',
-      'id2',
-    ],
-  },
-  id2: {
-    name: 'asdasd',
-    type: 'folder',
-    description: 'sdfsdfsd',
-    children: [
-      'id4',
-      'id6',
-    ],
-  },
-  id3: {
-    name: 'asdasd',
-    type: 'file',
-    description: 'sdfsdfsd',
-  },
-}*/
 
 /***/ }),
 
